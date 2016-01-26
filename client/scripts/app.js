ac68cc76122904dcf891dffa9950e8900892a16a
Justin Bitely <jbitely@gmail.com>
2016-01-25 23:29:43 -0500

    Implemented POST with form submission.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,7 @@ var mainSettings = {
 }
 
 var messageCache = {};
-
+var user = window.location.search.slice(10);
 var getMessages = $.ajax({
     url: mainSettings.endpoint,
     type: 'GET',
@@ -29,3 +29,32 @@ getMessages.done(function(){
     $('.content').append("<div>"+message.text+"</div>");
   });
 });
+
+$(document).ready(function(){
+  $('.input button').on('click',function(event){
+    event.preventDefault();
+    //create message
+    var message = {
+      roomname: 'default',
+      username: 'default',
+      text: $('.input textarea').val()
+    }
+    // post message
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: mainSettings.endpoint,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent. Data: ', data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message. Error: ', data);
+      }
+    });
+  })
+});
+
+
