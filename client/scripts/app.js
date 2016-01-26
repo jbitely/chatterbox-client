6944ef035d2251f34576f465b361422e23991f8d
Justin Bitely <jbitely@gmail.com>
2016-01-25 22:28:20 -0500

    Created global settings object, filter GET by creation date and limit.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,16 @@
+var mainSettings = {
+  limit: 50,
+  createdAfter: JSON.stringify({createdAt:{"$gte":{"__type":"Date","iso":"2016-01-24T00:00:00"}}}),
+  endpoint:'https://api.parse.com/1/classes/chatterbox',
+  room: 'default'
+
+}
 
 var messageCache = [];
 var getMessages = $.ajax({
-    url:'https://api.parse.com/1/classes/chatterbox',
+    url: mainSettings.endpoint,
     type: 'GET',
+    data: {limit: mainSettings.limit, where: mainSettings.createdAfter},
     success: function(response){
       for (var i = 0; i < response.results.length; i++) {
         messageCache.push(response.results[i]);
@@ -12,4 +20,9 @@ var getMessages = $.ajax({
       console.log('There was an error');
     }
   });
-getMessages.done(function(){console.log(messageCache);});
+
+getMessages.done(function(){
+  for(var i = 0; i< mainSettings.limit; i++){
+    $('.content').append("<div>"+messageCache[i].text+"</div>");
+  }
+});
