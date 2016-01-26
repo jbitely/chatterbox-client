bdc411ac07a8f6b521762deb874bd2c2b0e9b30d
Justin Bitely <jbitely@gmail.com>
2016-01-25 20:18:04 -0500

    Implemented basic GET for messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,15 @@
 
+var messageCache = [];
+var getMessages = $.ajax({
+    url:'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function(response){
+      for (var i = 0; i < response.results.length; i++) {
+        messageCache.push(response.results[i]);
+      };
+    },
+    error: function(){
+      console.log('There was an error');
+    }
+  });
+getMessages.done(function(){console.log(messageCache);});
