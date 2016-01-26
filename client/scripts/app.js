0613f2160b61850253cf4ebbf7c3bb4310942c38
Justin Bitely <jbitely@gmail.com>
2016-01-26 12:59:43 -0500

    Started refactoring to match test specs.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,45 +1,18 @@
-var mainSettings = {
-  limit: 50,
-  createdAfter: JSON.stringify({createdAt:{"$gte":{"__type":"Date","iso":"2016-01-24T00:00:00"}}}),
-  endpoint:'https://api.parse.com/1/classes/chatterbox',
-  room: 'default'
-
-}
-
-var messageCache = {};
-var user = window.location.search.slice(10);
-var getMessages = $.ajax({
-    url: mainSettings.endpoint,
-    type: 'GET',
-    data: {limit: mainSettings.limit, where: mainSettings.createdAfter},
-    success: function(response){
-      for (var i = 0; i < response.results.length; i++) {
-        if(!(response.results[i] in messageCache)){
-          messageCache[response.results[i].objectId] = response.results[i];
-        }
-      };
-    },
-    error: function(){
-      console.log('There was an error');
-    }
-  });
-
-getMessages.done(function(){
-  _.each(messageCache, function(message){
-    $('.content').append("<div>"+message.text+"</div>");
-  });
-});
+var app = {};
 
 $(document).ready(function(){
-  $('.input button').on('click',function(event){
-    event.preventDefault();
-    //create message
-    var message = {
-      roomname: 'default',
-      username: 'default',
-      text: $('.input textarea').val()
-    }
-    // post message
+
+  var mainSettings = {
+    limit: 50,
+    createdAfter: JSON.stringify({createdAt:{"$gte":{"__type":"Date","iso":"2016-01-24T00:00:00"}}}),
+    endpoint:'https://api.parse.com/1/classes/chatterbox',
+    room: 'default'
+  }
+  var messageCache = {};
+  var user = window.location.search.slice(10);
+
+  app.init = function(){return};
+  app.send = function(message){
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: mainSettings.endpoint,
@@ -54,7 +27,42 @@ $(document).ready(function(){
         console.error('chatterbox: Failed to send message. Error: ', data);
       }
     });
+  };
+
+  var getMessages = $.ajax({
+      url: mainSettings.endpoint,
+      type: 'GET',
+      data: {limit: mainSettings.limit, where: mainSettings.createdAfter},
+      success: function(response){
+        for (var i = 0; i < response.results.length; i++) {
+          if(!(response.results[i] in messageCache)){
+            messageCache[response.results[i].objectId] = response.results[i];
+          }
+        };
+      },
+      error: function(){
+        console.log('There was an error');
+      }
+    });
+
+  getMessages.done(function(){
+    _.each(messageCache, function(message){
+      $('.content').append("<div>"+message.text+"</div>");
+    });
+  });
+
+  $('.input button').on('click',function(event){
+    event.preventDefault();
+    //create message
+    var message = {
+      roomname: 'default',
+      username: 'default',
+      text: $('.input textarea').val()
+    }
+    // post message
+    app.send(message);
   })
+
   $('.rooms').change(function(){
     var selected = $('.rooms option:selected').val();
     if(selected === 'newRoom'){
